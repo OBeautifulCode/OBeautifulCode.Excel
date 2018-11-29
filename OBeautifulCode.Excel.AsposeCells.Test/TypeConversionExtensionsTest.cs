cb2251ce66317a8b94a350f27a378e37aa7b380f
Suraj Gupta <SurajGupta@users.noreply.github.com>
2018-11-29 13:56:29 -0500

    added ToBorderType extension method

---
+++ OBeautifulCode.Excel.AsposeCells.Test/TypeConversionExtensionsTest.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeConversionExtensionsTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel.AsposeCells.Test
+{
+    using System;
+    using System.Linq;
+
+    using Aspose.Cells;
+
+    using FluentAssertions;
+
+    using OBeautifulCode.Enum.Recipes;
+
+    using Xunit;
+
+    public static class TypeConversionExtensionsTest
+    {
+        [Fact]
+        public static void ToBorderType__Should_throw_ArgumentOutOfRangeException___When_parameter_borderEdges_is_Unknown()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => BorderEdges.Unknown.ToBorderType());
+
+            // Assert
+            actual.Should().BeOfType<ArgumentOutOfRangeException>();
+            actual.Message.Should().Contain(nameof(BorderEdges.Unknown));
+        }
+
+        [Fact]
+        public static void ToBorderType__Should_convert_borderEdges_to_a_BorderType___When_borderEdges_has_single_flag()
+        {
+            // Arrange
+            var flags = EnumExtensions.GetIndividualFlags<BorderEdges>().Skip(1).ToList();
+            var expected = flags.Select(_ => Enum.Parse(typeof(BorderType), _.ToString())).ToList();
+
+            // Act
+            var actual = flags.Select(_ => _.ToBorderType()).ToList();
+
+            // Act
+            actual.Should().Equal(expected);
+        }
+
+        [Fact]
+        public static void ToBorderType__Should_convert_borderEdges_to_a_BorderType___When_borderEdges_has_multiple_flags()
+        {
+            // Arrange
+            var borderEdges1 = BorderEdges.DiagonalDown | BorderEdges.TopBorder;
+            var borderEdges2 = BorderEdges.Outline;
+
+            var expected1 = BorderType.DiagonalDown | BorderType.TopBorder;
+            var expected2 = BorderType.BottomBorder | BorderType.TopBorder | BorderType.LeftBorder | BorderType.RightBorder;
+
+            // Act
+            var actual1 = borderEdges1.ToBorderType();
+            var actual2 = borderEdges2.ToBorderType();
+
+            // Act
+            actual1.Should().Be(expected1);
+            actual2.Should().Be(expected2);
+        }
+    }
+}
--- OBeautifulCode.Excel.AsposeCells/TypeConversionExtensions.cs
+++ OBeautifulCode.Excel.AsposeCells/TypeConversionExtensions.cs
@@ -6,12 +6,60 @@
 
 namespace OBeautifulCode.Excel.AsposeCells
 {
+    using Aspose.Cells;
+
+    using OBeautifulCode.Validation.Recipes;
+
     /// <summary>
     /// Contains extension methods to convert to/from OBeautifulCode.Excel and Aspose.Cells types.
     /// </summary>
-    internal class TypeConversionExtensions
+    public static class TypeConversionExtensions
     {
-        // public BorderType ToBorderType(
-        //    BorderEdges borderEdges)
+        /// <summary>
+        /// Converts a <see cref="BorderEdges"/> to a <see cref="BorderType"/>.
+        /// </summary>
+        /// <param name="borderEdges">The border edges to convert.</param>
+        /// <returns>
+        /// A <see cref="BorderType"/> converted from a <see cref="BorderEdges"/>.
+        /// </returns>
+        public static BorderType ToBorderType(
+            this BorderEdges borderEdges)
+        {
+            new { borderEdges }.Must().NotBeEqualTo(BorderEdges.Unknown);
+
+            var result = default(BorderType);
+
+            if (borderEdges.HasFlag(BorderEdges.BottomBorder))
+            {
+                result |= BorderType.BottomBorder;
+            }
+
+            if (borderEdges.HasFlag(BorderEdges.DiagonalDown))
+            {
+                result |= BorderType.DiagonalDown;
+            }
+
+            if (borderEdges.HasFlag(BorderEdges.DiagonalUp))
+            {
+                result |= BorderType.DiagonalUp;
+            }
+
+            if (borderEdges.HasFlag(BorderEdges.LeftBorder))
+            {
+                result |= BorderType.LeftBorder;
+            }
+
+            if (borderEdges.HasFlag(BorderEdges.RightBorder))
+            {
+                result |= BorderType.RightBorder;
+            }
+
+            if (borderEdges.HasFlag(BorderEdges.TopBorder))
+            {
+                result |= BorderType.TopBorder;
+            }
+
+            return result;
+        }
     }
 }
