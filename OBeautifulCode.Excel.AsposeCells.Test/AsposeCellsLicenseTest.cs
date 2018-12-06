5f2feaae86cc607146f5288370dea1348cf94be2
Suraj Gupta <SurajGupta@users.noreply.github.com>
2018-12-06 13:33:01 -0500

    added AsposeCellsLicense

---
+++ OBeautifulCode.Excel.AsposeCells.Test/AsposeCellsLicenseTest.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AsposeCellsLicenseTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel.AsposeCells.Test
+{
+    using System;
+
+    using FakeItEasy;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public static class AsposeCellsLicenseTest
+    {
+        [Fact]
+        public static void Constructor___Should_throw_ArgumentNullException___When_parameter_licenseXml_is_null()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => new AsposeCellsLicense(null));
+
+            // Assert
+            actual.Should().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("licenseXml");
+        }
+
+        [Fact]
+        public static void Constructor___Should_throw_ArgumentException___When_parameter_licenseXml_is_white_space()
+        {
+            // Arrange, Act
+            var actual = Record.Exception(() => new AsposeCellsLicense(" \r\n "));
+
+            // Assert
+            actual.Should().BeOfType<ArgumentException>();
+            actual.Message.Should().Contain("licenseXml");
+            actual.Message.Should().Contain("white space");
+        }
+
+        [Fact]
+        public static void Register___Should_throw_InvalidOperationException___When_licenseXml_is_invalid_or_corrupt()
+        {
+            // Arrange
+            var systemUnderTest1 = new AsposeCellsLicense(A.Dummy<string>());
+            var systemUnderTest2 = new AsposeCellsLicense("<License></License>");
+
+            // Act
+            var actual1 = Record.Exception(() => systemUnderTest1.Register());
+            var actual2 = Record.Exception(() => systemUnderTest2.Register());
+
+            // Assert
+            actual1.Should().BeOfType<InvalidOperationException>();
+            actual1.Message.Should().Contain("LicenseXml is invalid or corrupt");
+
+            actual2.Should().BeOfType<InvalidOperationException>();
+            actual2.Message.Should().Contain("LicenseXml is invalid or corrupt");
+        }
+    }
+}
---
+++ OBeautifulCode.Excel.AsposeCells/AsposeCellsLicense.cs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AsposeCellsLicense.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel.AsposeCells
+{
+    using System;
+    using System.IO;
+
+    using Aspose.Cells;
+
+    using OBeautifulCode.String.Recipes;
+    using OBeautifulCode.Validation.Recipes;
+
+    using static System.FormattableString;
+
+    /// <summary>
+    /// The Aspose.Cells license.
+    /// </summary>
+    public class AsposeCellsLicense
+    {
+        private static readonly object RegisterLock = new object();
+
+        private static bool hasBeenRegistered = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsposeCellsLicense"/> class.
+        /// </summary>
+        /// <param name="licenseXml">The license XML.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="licenseXml"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="licenseXml"/> is white space.</exception>
+        public AsposeCellsLicense(
+            string licenseXml)
+        {
+            new { licenseXml }.Must().NotBeNullNorWhiteSpace();
+
+            this.LicenseXml = licenseXml;
+        }
+
+        /// <summary>
+        /// Gets the license XML.
+        /// </summary>
+        public string LicenseXml { get; }
+
+        /// <summary>
+        /// Registers the license.
+        /// </summary>
+        /// <remarks>
+        /// This method ensures that the license is only registered once per appdomain.
+        /// </remarks>
+        /// <exception cref="InvalidOperationException"><see cref="LicenseXml"/> is invalid or corrupt.</exception>
+        public void Register()
+        {
+            lock (RegisterLock)
+            {
+                if (!hasBeenRegistered)
+                {
+                    var license = new License();
+
+                    try
+                    {
+                        using (var ms = new MemoryStream(this.LicenseXml.ToUtf8Bytes()))
+                        {
+                            license.SetLicense(ms);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new InvalidOperationException(Invariant($"{nameof(this.LicenseXml)} is invalid or corrupt.  See inner exception."), ex);
+                    }
+
+                    hasBeenRegistered = true;
+                }
+            }
+        }
+    }
+}
