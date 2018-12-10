7885f835ac0b7fa85b44c5aa232b99724e5efcef
Suraj Gupta <SurajGupta@users.noreply.github.com>
2018-12-10 16:26:39 -0500

    add AsposeCellsDummyFactory

---
+++ OBeautifulCode.Excel.AsposeCells.Test/AsposeCellsDummyFactory.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AsposeCellsDummyFactory.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel.AsposeCells.Test
+{
+    using System;
+
+    using Aspose.Cells;
+
+    using FakeItEasy;
+
+    using OBeautifulCode.AutoFakeItEasy;
+
+    /// <inheritdoc />
+    public class AsposeCellsDummyFactory : IDummyFactory
+    {
+        public AsposeCellsDummyFactory()
+        {
+            AutoFixtureBackedDummyFactory.AddDummyCreator(() =>
+            {
+                var workbook = new Workbook();
+
+                var result = workbook.Worksheets[0];
+
+                return result;
+            });
+        }
+
+        /// <inheritdoc />
+        public Priority Priority => new FakeItEasy.Priority(1);
+
+        /// <inheritdoc />
+        public bool CanCreate(Type type)
+        {
+            return false;
+        }
+
+        /// <inheritdoc />
+        public object Create(Type type)
+        {
+            return null;
+        }
+    }
+}
