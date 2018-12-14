b44d1c674340cb7a1c0c4c676d9f08cc32212b69
Suraj Gupta <SurajGupta@users.noreply.github.com>
2018-12-14 13:35:30 -0500

    added Constants

---
+++ OBeautifulCode.Excel/Constants.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Constants.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel
+{
+    /// <summary>
+    /// Contains various constants.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Specifies the maximum row height, in pixels.
+        /// </summary>
+        public const int MaxRowHeightInPixels = 454;
+    }
+}
