eb223e9a51fb54391aadd3cba28725a35f75e675
Suraj Gupta <SurajGupta@users.noreply.github.com>
2019-03-07 14:50:00 -0500

    added method to get column name from column number

---
+++ OBeautifulCode.Excel.Test/CellsHelperTest.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CellsHelperTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public static class CellsHelperTest
+    {
+        [Fact]
+        public static void GetColumnName___Should_throw_ArgumentOutOfRangeException___When_parameter_columnNumber_is_less_than_1()
+        {
+            // Arrange
+            var columnNumbers = new[] { 0, -1, int.MinValue };
+
+            // Act
+            var actuals = columnNumbers.Select(_ => Record.Exception(() => CellsHelper.GetColumnName(_))).ToList();
+
+            // Assert
+            foreach (var actual in actuals)
+            {
+                actual.Should().BeOfType<ArgumentOutOfRangeException>();
+                actual.Message.Should().Contain("columnNumber");
+            }
+        }
+
+        [Fact]
+        public static void GetColumnName___Should_throw_ArgumentOutOfRangeException___When_parameter_columnNumber_is_greater_than_Constants_MaximumColumnNumber()
+        {
+            // Arrange
+            var columnNumbers = new[] { Constants.MaximumColumnNumber + 1, int.MaxValue };
+
+            // Act
+            var actuals = columnNumbers.Select(_ => Record.Exception(() => CellsHelper.GetColumnName(_))).ToList();
+
+            // Assert
+            foreach (var actual in actuals)
+            {
+                actual.Should().BeOfType<ArgumentOutOfRangeException>();
+                actual.Message.Should().Contain("columnNumber");
+            }
+        }
+
+        [Fact]
+        public static void GetColumnName___Should_return_column_name___When_called()
+        {
+            // Arrange
+            var columnNumberToExpectedColumnNameMap = new Dictionary<int, string>
+            {
+                { 1, "A" },
+                { 2, "B" },
+                { 26, "Z" },
+                { 27, "AA" },
+                { 52, "AZ" },
+                { 53, "BA" },
+                { 701, "ZY" },
+                { 702, "ZZ" },
+                { 703, "AAA" },
+                { 704, "AAB" },
+                { 10340, "OGR" },
+                { 16384, "XFD" },
+            };
+
+            var expected = columnNumberToExpectedColumnNameMap.OrderBy(_ => _.Key).Select(_ => _.Value);
+
+            // Act
+            var actual = columnNumberToExpectedColumnNameMap.OrderBy(_ => _.Key).Select(_ => CellsHelper.GetColumnName(_.Key)).ToList();
+
+            // Assert
+            expected.Should().Equal(actual);
+        }
+    }
+}
---
+++ OBeautifulCode.Excel/Cell/CellsHelper.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CellsHelper.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel
+{
+    using OBeautifulCode.Validation.Recipes;
+
+    /// <summary>
+    /// Helper methods related to cells.
+    /// </summary>
+    public static class CellsHelper
+    {
+        /// <summary>
+        /// Gets the column name for the specified 1-based column number.
+        /// </summary>
+        /// <param name="columnNumber">The 1-based column number.</param>
+        /// <returns>
+        /// The column name.
+        /// </returns>
+        public static string GetColumnName(
+            int columnNumber)
+        {
+            new { columnNumber }.Must().BeGreaterThanOrEqualTo(1).And().BeLessThanOrEqualTo(Constants.MaximumColumnNumber);
+
+            var result = GetColumnNameInternal(columnNumber);
+
+            return result;
+        }
+
+        private static string GetColumnNameInternal(
+            int columnNumber)
+        {
+            if (columnNumber < 1)
+            {
+                return string.Empty;
+            }
+
+            var result = GetColumnNameInternal((columnNumber - 1) / 26) + (char)('A' + ((columnNumber - 1) % 26));
+
+            return result;
+        }
+    }
+}
