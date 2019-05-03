0e7042a520ad5c3e519cd197f4db3f05fa92a76e
Suraj Gupta <SurajGupta@users.noreply.github.com>
2019-05-03 11:53:12 -0400

    added GetCell(this CellReference, Workbook workbook)

---
+++ OBeautifulCode.Excel.AsposeCells.Test/Read/CellReferenceExtensionsTest.Read.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CellReferenceExtensionsTest.Read.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel.AsposeCells.Test
+{
+    using System;
+
+    using Aspose.Cells;
+
+    using FakeItEasy;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public static partial class CellReferenceExtensionsTest
+    {
+        [Fact]
+        public static void GetCell___Should_throw_ArgumentNullException___When_parameter_cellReference_is_null()
+        {
+            // Arrange
+            var workbook = A.Dummy<Worksheet>().Workbook;
+
+            // Act
+            var actual = Record.Exception(() => CellReferenceExtensions.GetCell(null, workbook));
+
+            // Assert
+            actual.Should().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("cellReference");
+        }
+
+        [Fact]
+        public static void GetCell___Should_throw_ArgumentNullException___When_parameter_workbook_is_null()
+        {
+            // Arrange
+            var cellReference = A.Dummy<CellReference>();
+
+            // Act
+            var actual = Record.Exception(() => cellReference.GetCell(null));
+
+            // Assert
+            actual.Should().BeOfType<ArgumentNullException>();
+            actual.Message.Should().Contain("workbook");
+        }
+
+        [Fact]
+        public static void GetCell___Should_return_cell_corresponding_to_cellReference___When_called()
+        {
+            // Arrange
+            var worksheet = A.Dummy<Worksheet>();
+
+            var expected = worksheet.Cells["C9"];
+
+            var systemUnderTest = expected.ToCellReference();
+
+            // Act
+            var actual = systemUnderTest.GetCell(worksheet.Workbook);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ OBeautifulCode.Excel.AsposeCells/Read/CellReferenceExtensions.Read.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CellReferenceExtensions.Read.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel.AsposeCells
+{
+    using System;
+
+    using Aspose.Cells;
+
+    using OBeautifulCode.Validation.Recipes;
+
+    /// <summary>
+    /// Extensions methods on type <see cref="CellReference"/>.
+    /// </summary>
+    public static partial class CellReferenceExtensions
+    {
+        /// <summary>
+        /// Gets a cell by it's reference.
+        /// </summary>
+        /// <param name="cellReference">The cell reference.</param>
+        /// <param name="workbook">The workbook.</param>
+        /// <returns>
+        /// The cell corresponding to the specified reference.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="cellReference"/> is null.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="workbook"/> is null.</exception>
+        public static Cell GetCell(
+            this CellReference cellReference,
+            Workbook workbook)
+        {
+            new { cellReference }.Must().NotBeNull();
+            new { workbook }.Must().NotBeNull();
+
+            var result = workbook.Worksheets[cellReference.WorksheetName].Cells[cellReference.RowNumber - 1, cellReference.ColumnNumber - 1];
+
+            return result;
+        }
+    }
+}
