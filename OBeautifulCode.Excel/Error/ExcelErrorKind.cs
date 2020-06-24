d1bd8193d07d3201884fd4e4e1fb45bc33462cbc
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-06-24 08:45:36 -0400

    add ExcelErrorKind/extensions

---
+++ OBeautifulCode.Excel/Error/ExcelErrorKind.cs
@@ -0,0 +1,95 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExcelErrorKind.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using OBeautifulCode.Excel.Internal;
+
+    /// <summary>
+    /// Specifies a kind of Excel error.
+    /// </summary>
+    /// <remarks>
+    /// We tried to get use more descriptive names (e.g. DivideByZero) but Microsoft
+    /// doesn't have any official documentation (or none we could find) for Excel errors
+    /// and anyways we thought it would be more straightforward to just use the error notation
+    /// which is familiar to Excel users.
+    ///
+    /// We set each enum value equal to what Excel returns when you call =ERROR.TYPE().
+    /// </remarks>
+    [SuppressMessage("Microsoft.Design", "CA1027:MarkEnumsWithFlags", Justification = ObcSuppressBecause.CA1027_MarkEnumsWithFlags_EnumValuesArePurposefullyNonContiguous)]
+    public enum ExcelErrorKind
+    {
+        /// <summary>
+        /// No error.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Error #NULL!
+        /// </summary>
+        Null = 1,
+
+        /// <summary>
+        /// Error #DIV/0!
+        /// </summary>
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Div", Justification = ObcSuppressBecause.CA1704_IdentifiersShouldBeSpelledCorrectly_SpellingIsCorrectInContextOfTheDomain)]
+        Div0 = 2,
+
+        /// <summary>
+        /// Error #VALUE!
+        /// </summary>
+        Value = 3,
+
+        /// <summary>
+        /// Error #REF!
+        /// </summary>
+        Ref = 4,
+
+        /// <summary>
+        /// Error #NAME?
+        /// </summary>
+        Name = 5,
+
+        /// <summary>
+        /// Error #NUM!
+        /// </summary>
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Num", Justification = ObcSuppressBecause.CA1704_IdentifiersShouldBeSpelledCorrectly_SpellingIsCorrectInContextOfTheDomain)]
+        Num = 6,
+
+        /// <summary>
+        /// Error  #N/A
+        /// </summary>
+        Na = 7,
+
+        /// <summary>
+        /// Error #GETTING_DATA
+        /// </summary>
+        GettingData = 8,
+
+        /// <summary>
+        /// Error #SPILL!
+        /// </summary>
+        Spill = 9,
+
+        /// <summary>
+        /// Error #UNKONWN!
+        /// </summary>
+        Unknown = 12,
+
+        /// <summary>
+        /// Error #FIELD!
+        /// </summary>
+        Field = 13,
+
+        /// <summary>
+        /// Error #CALC!
+        /// </summary>
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Calc", Justification = ObcSuppressBecause.CA1704_IdentifiersShouldBeSpelledCorrectly_SpellingIsCorrectInContextOfTheDomain)]
+        Calc = 14,
+    }
+}
---
+++ OBeautifulCode.Excel/Error/ExcelErrorKindExtensions.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExcelErrorKindExtensions.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Excel
+{
+    using System;
+
+    /// <summary>
+    /// Extensions on <see cref="ExcelErrorKind"/>.
+    /// </summary>
+    public static class ExcelErrorKindExtensions
+    {
+        /// <summary>
+        /// Gets the Excel identifier for the specified kind of error.
+        /// </summary>
+        /// <param name="errorKind">The kind of error.</param>
+        /// <returns>
+        /// The Excel identifier for the specified kind of error.
+        /// </returns>
+        public static string ToExcelIdentifier(
+            this ExcelErrorKind errorKind)
+        {
+            switch (errorKind)
+            {
+                case ExcelErrorKind.None:
+                    return null;
+                case ExcelErrorKind.Null:
+                    return "#NULL!";
+                case ExcelErrorKind.Div0:
+                    return "#DIV/0!";
+                case ExcelErrorKind.Value:
+                    return "#VALUE!";
+                case ExcelErrorKind.Ref:
+                    return "#REF!";
+                case ExcelErrorKind.Name:
+                    return "#NAME?";
+                case ExcelErrorKind.Num:
+                    return "#NUM!";
+                case ExcelErrorKind.Na:
+                    return "#N/A";
+                case ExcelErrorKind.GettingData:
+                    return "#GETTING_DATA";
+                case ExcelErrorKind.Spill:
+                    return "#SPILL!";
+                case ExcelErrorKind.Unknown:
+                    return "#UNKNOWN!";
+                case ExcelErrorKind.Field:
+                    return "#FIELD!";
+                case ExcelErrorKind.Calc:
+                    return "#CALC!";
+                default:
+                    throw new NotSupportedException("This error kind is not supported: " + errorKind);
+            }
+        }
+    }
+}
